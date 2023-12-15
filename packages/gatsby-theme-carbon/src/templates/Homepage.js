c8c3fa27962ec914d8b934ac7b7eb0ca837a1f3d
Oz-Type <153376108+Oz-Type@users.noreply.github.com>
2023-12-15 12:19:19 +0100

    Update Homepage.js

--- packages/gatsby-theme-carbon/src/templates/Homepage.js
+++ packages/gatsby-theme-carbon/src/templates/Homepage.js
@@ -3,49 +3,14 @@ import Layout from '../components/Layout';
 import Main from '../components/Main';
 import useMetadata from '../util/hooks/useMetadata';
 import Utils from '../components/Utils';
-import { HomepageCallout } from 'gatsby-theme-carbon';
-import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
-import HomepageVideo from '../components/HomepageVideo';
+
 import NextPrevious from '../components/NextPrevious';
-import { gray10 } from '@carbon/colors';
 
-// Component to be shadowed
-const FirstLeftText = () => (
-  <p>
-    Think → <em>Guide</em>
-  </p>
-);
-
-const FirstRightText = () => (
-  <p>
-    Build Bonds
-    <br />
-    This is the guiding ethos behind IBM’s design philosophy and principles.
-    This helps us distinguish every element and every experience Designed&nbsp;by&nbsp;IBM.
-    <span style={{ paddingTop: "1.25rem", display : "flex" }}>↓</span>
-  </p>
-);
-
-const customProps = {
-  Banner: (
-    <HomepageVideo
-      src="videos/hero-video-min.mp4"
-      poster="images/hero-video-poster.jpg"
-    />
-  ),
-  FirstCallout: (
-    <HomepageCallout
-      backgroundColor={gray10}
-      color={"#191919"}
-      leftText={FirstLeftText}
-      rightText={FirstRightText}
-    />
-  ),
-  SecondCallout: null,
-};
-
-const MergedHomepage = ({
+const Homepage = ({
   children,
+  Banner,
+  FirstCallout,
+  SecondCallout,
   location,
   pageContext,
 }) => {
@@ -61,19 +26,14 @@ const MergedHomepage = ({
       titleType={titleType}
       homepage
       theme={homepageTheme}>
-      {customProps.Banner}
-      {customProps.FirstCallout}
+      {Banner}
+      {FirstCallout}
       <Main>{children}</Main>
-      {customProps.SecondCallout}
+      {SecondCallout}
       <NextPrevious isHomepage location={location} pageContext={pageContext} />
       <Utils />
     </Layout>
   );
 };
 
-// spreading the original props gives us props.children (mdx content)
-function ShadowedHomepage(props) {
-  return <HomepageTemplate {...props} {...customProps} />;
-}
-
-export default MergedHomepage;
+export default Homepage;
