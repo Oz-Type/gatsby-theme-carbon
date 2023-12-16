65cd2dba965c9ab95de368316b06a0e68f54eee6
Oz-Type <153376108+Oz-Type@users.noreply.github.com>
2023-12-16 17:12:13 +0100

    Update Homepage.js

--- packages/example/src/components/Homepage.js
+++ packages/example/src/components/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
 import { calloutLink } from './Homepage.module.scss';
+import { gray10 } from '@carbon/colors';
 
 import Carbon from './carbon.jpg';
 
